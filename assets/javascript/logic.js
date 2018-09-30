e3f0161ff8a06db3964904b84f59e8fe7c7f6be9
Eduardo Monforte <eduardomonforte@gmail.com>
2018-09-30 01:11:38 -0500

    DONE.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -25,7 +25,27 @@ $(document).ready(function() {
     })
     
     // When a button is clicked, the showGif function is called.
-    $(document).on("click", ".button" , showGif);
+    $(document).on("click", ".button", showGif);
+
+    // When a gif is clicked, it is either paused or played.
+    $(document).on("click", ".gif", function() {
+        // We store the current state in a variable.
+        var state = $(this).attr("data-state");
+        // If the state is "still"...
+        if (state === "still") {
+            // ...change the source to the animated url...
+            $(this).attr("src", $(this).attr("data-animate"));
+            // ...and change the state to "animate".
+            $(this).attr("data-state", "animate");
+        }
+        // If the state is "animate"...
+        if (state === "animate") {
+            // ...change the source to the still url...
+            $(this).attr("src", $(this).attr("data-still"));
+            // ...and change the state to "still".
+            $(this).attr("data-state", "still");
+        }
+    });
 
     // FUNCTIONS
 
@@ -63,7 +83,7 @@ $(document).ready(function() {
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            // The response to the request is logged into the console. Useful for debugging.
+            // We log the response into the console. Useful for debugging.
             console.log(response);
             // We clear the div containing the gifs so the new ones don't stack.
             $("#gifs-box").empty();
@@ -73,25 +93,34 @@ $(document).ready(function() {
             for (var i = 0; i < results.length; i++) {
                 // We create new divs for each new gif.
                 var gifDiv = $("<div>");
-                // We create new paragraphs showcasing the rating for the gifs.
-                var p = $("<p>").text("Rating: " + results[i].rating.toUpperCase());
-                // We create a new line break so it looks nicer.
-                var lineBreak = $("<br>");
+                // We create new paragraphs showcasing the ratings of the gifs.
+                var rating = $("<p>").text("Rating: " + results[i].rating.toUpperCase());
                 // We create new image elements for each gif.
                 var newGifs = $("<img>");
                 // We set the source for each gif using dot notation.
-                newGifs.attr("src", results[i].images.fixed_height.url);
+                newGifs.attr("src", results[i].images.original.url);
+                // We define the default state of the gif as "animate"
+                newGifs.attr("data-state", "animate")
+                // We define the animated url of the gif inside of an attribute.
+                newGifs.attr("data-animate", results[i].images.original.url)
+                // We define the still url of the gif inside of an attribute.
+                newGifs.attr("data-still", results[i].images.original_still.url)
+                // We give the class "gif" to every new gif. Useful later on.
+                newGifs.attr("class", "gif");
                 // We set some attributes that control the size of the gifs so they look nicer.
                 newGifs.attr("width", "400px");
                 newGifs.attr("height", "auto");
-                // We append the paragraph with the rating to the new divs.
-                gifDiv.append(p);
-                // We append the line break to the new divs.
-                gifDiv.append(lineBreak);
+                // We append the paragraphs with the ratings to the new divs.
+                gifDiv.append(rating);
+                // We append a line break to the new divs.
+                gifDiv.append("<br>");
                 // We append the gifs to the new divs.
                 gifDiv.append(newGifs);
-                // We prepend the new divs to the HTML div where they belong.
-                $("#gifs-box").prepend(gifDiv);
+                // We append some additional line breaks to the new divs.
+                gifDiv.append("<br>");
+                gifDiv.append("<br>");
+                // We append the new divs to the HTML div where they belong.
+                $("#gifs-box").append(gifDiv);
             }
         });
     }
